6d8ce85421e2dc88a666e83f59b34f065466d1d7
Gopal Singh <sinhhgopal914@gmail.com>
2024-02-16 19:44:20 +0530

    Create db.js

---
+++ config/db.js
@@ -0,0 +1,28 @@
+// const mongoose = require('mongoose');
+
+// const connectDB = async () => {
+//   try {
+//     await mongoose.connect(process.env.MONGODB_URI);
+//     console.log(`MongoDB Connected: ${mongoose.connection.host}`);
+//   } catch (error) {
+//     console.error(error);
+//     throw error; // Re-throw the error to be handled in server.js
+//   }
+// };
+
+// module.exports = connectDB;
+
+import mongoose from 'mongoose';
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log(`MongoDB Connected: ${mongoose.connection.host}`);
+  } catch (error) {
+    console.error(error);
+    throw error; // Re-throw the error to be handled in server.js
+  }
+};
+
+// Export the named function
+export default connectDB;
